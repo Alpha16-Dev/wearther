5cb5608a52c43200978f73ff6d0e4b7623c1aa71
Alpha16-Dev <mkhatshwaalpha@gmail.com>
2025-09-16 22:58:13 +0200

    Initial Wearther app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Type a city, get the 7-day forecast, and smart outfit suggestions.")
 # --- Helpers ---
 GEO_URL = "https://geocoding-api.open-meteo.com/v1/search"
 WX_URL = "https://api.open-meteo.com/v1/forecast"
-
+# test
 def geocode_city(city: str):
     """Use Open-Meteo Geocoding to turn a city name into lat/lon."""
     r = requests.get(GEO_URL, params={"name": city, "count": 1, "language": "en", "format": "json"}, timeout=10)
